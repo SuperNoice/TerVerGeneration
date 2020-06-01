0a7eae3059a880637d0c010281010777f38591e9
Andrey <galan.andrey@gmail.com>
2020-06-01 21:17:35 +0300

    Add 16 18

--- ГенерацияТВ/Form1.cs
+++ ГенерацияТВ/Form1.cs
@@ -50,17 +50,18 @@ namespace ГенерацияТВ
             Paragraph paragraph;
             randBuff buff = new randBuff();
             Random r;
-
+            IExcel.Application excel;
             public Gen(int countVariants)
             {
                 r = new Random(System.DateTime.Now.Millisecond);
+                excel = new IExcel.Application();
                 SaveFileDialog saveFile = new SaveFileDialog();
                 saveFile.DefaultExt = ".docx";
                 saveFile.AddExtension = true;
                 saveFile.Title = "Сохранить как...";
                 saveFile.OverwritePrompt = true;
                 saveFile.Filter = "Text files(*.docx)|*.docx|All files(*.*)|*.*";
-                
+
                 if (saveFile.ShowDialog() == DialogResult.Cancel)
                     return;
 
@@ -106,7 +107,7 @@ namespace ГенерацияТВ
             {
                 int res;
 
-                if (to - from < 4)
+                if (to - from + 1 < 4)
                 {
                     do
                     {
@@ -139,15 +140,16 @@ namespace ГенерацияТВ
                 int[] mas = new int[4] { 10, 20, 25, 50};
                
                 all = mas[r.Next(0, 3)];
-                part1 = r.Next(3, all - 1);                            
+                part1 = r.Next(3, all - 3);                            
                 part2 = all - part1;
                 part3 = r.Next(4, all/2);
-                quest = r.Next(2, part1 > part3 ? part3 - 2 : part1 - 1);
+                quest = r.Next(part2 < part3 ? part3 - part2:2, part1 > part3 ? part3 - 2 : part1 - 1);
 
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("2.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
                 paragraph.Append("В урне " + part1.ToString() +" белых и "+ part2.ToString() +" черных шаров. Наудачу отобраны "+ part3.ToString() +" шаров. Найти вероятность того, что среди них окажется ровно "+ quest.ToString() +" белых шаров.").Font("Century Schoolbook").FontSize(12);
-                // otvetC( part1,quest)*C(part2,part3 - quest)/C(all,part3)
+                double result = (double)excel.WorksheetFunction.Combin(part1, quest) * (double)excel.WorksheetFunction.Combin(part2, part3 - quest) / (double)excel.WorksheetFunction.Combin(all, part3);
+                if (result > 1) MessageBox.Show("Говно");
             }
 
             private void gen3()
@@ -172,7 +174,8 @@ namespace ГенерацияТВ
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("4.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
                 paragraph.Append("В партии готовой продукции, состоящей из " + all.ToString() + " изделий, " + part2.ToString() + " брако­ванных. Найти вероятность того, что при случайном выборе " + quest.ToString() + " изделий число бракованных и не бракованных изделий окажется поровну.").Font("Century Schoolbook").FontSize(12);
-                // otvetC( part1,quest/2)*C(part2, quest/2)/C(all, quest)
+                double result = (double)excel.WorksheetFunction.Combin(part1, quest / 2) * (double)excel.WorksheetFunction.Combin(part2, quest / 2) / (double)excel.WorksheetFunction.Combin(all, quest);
+                if (result > 1) MessageBox.Show("Говно");
             }
 
             private void gen5()
@@ -194,7 +197,8 @@ namespace ГенерацияТВ
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("6.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
                 paragraph.Append("Произведен залп из двух орудий. Вероятность попадания из первого орудия равна " + part1.ToString() + ", из второго " + part2.ToString() + ". Найти вероятность поражения цели.").Font("Century Schoolbook").FontSize(12);
-                //otvet = 1-(1-part1)*(1-part2)
+                double result = 1 - (1 - part1) * (1 - part2);
+                if (result > 1) MessageBox.Show("Говно");
             }
 
             private void gen7()
@@ -215,7 +219,8 @@ namespace ГенерацияТВ
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("8.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
                 paragraph.Append("Рабочий обслуживает 3 автомата. Вероятность брака для пер­вого автомата равна " + part1.ToString() + "; для второго " + part2.ToString() + "; для третьего "+part3.ToString()+ ". Производи­тельность всех автоматов одинакова. Изготовленные детали попадают на общий конвейер. Определить вероятность того, что взятая наугад деталь будет годной.").Font("Century Schoolbook").FontSize(12);
-                //otvet= 1-(1/3*part1+1/3*part2+1/3*part3)
+                double result = 1 - (1 / 3 * part1 + 1 / 3 * part2 + 1 / 3 * part3);
+                if (result > 1) MessageBox.Show("Говно");
             }
 
             private void gen9()
@@ -237,8 +242,9 @@ namespace ГенерацияТВ
 
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("10.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
-                paragraph.Append("Рабочий обслуживает 3 автомата. Вероятность брака для пер­вого автомата равна " + part1.ToString() + "; для второго " + part2.ToString() + "; для третьего " + part3.ToString() + ". Производи­тельность всех автоматов одинакова. Изготовленные детали попадают на общий конвейер. Определить вероятность того, что взятая наугад деталь будет годной.").Font("Century Schoolbook").FontSize(12);
-                //otvet = C(part1,part2)*pow(part3,part2)*pow(1-part3,part1-part2)
+                paragraph.Append("Вероятность выигрыша по облигации займа равна " + part3.ToString() + ". Какова вероятность того, что из " + part1.ToString() + " взятых облигаций " + part2.ToString() + " выиграют?").Font("Century Schoolbook").FontSize(12);
+                double result = (double)excel.WorksheetFunction.Combin(part1, part2) * Math.Pow(part3, part2) * Math.Pow(1 - part3, part1 - part2);
+                if (result > 1) MessageBox.Show("Говно");
             }
 
             private void gen11()
@@ -248,13 +254,14 @@ namespace ГенерацияТВ
 
 
             }
-
+            
             private void gen12()
             {
+               
+           
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("12.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
-
-
+                
             }
 
             private void gen13()
@@ -283,9 +290,16 @@ namespace ГенерацияТВ
 
             private void gen16()
             {
+                double a, q;
+
+                a = (double)randInt(5, 20);
+                q = (double)randInt(1, (int)a) / 10d;
+                a /= 10d;
+                double result = (excel.WorksheetFunction.NormSDist((1 - a) / q) - 0.5) - (excel.WorksheetFunction.NormSDist((0.3 - a) / q) - 0.5);
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("16.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
-
+                paragraph.Append(" E - нормально распределенная случайная величина с парамет­рами а=" + a.ToString() + "  q=" + q.ToString() + ".  Найти Р(0,3<E<1).").FontSize(12);
+                if (result > 1) MessageBox.Show("Говно");
 
             }
 
@@ -293,14 +307,43 @@ namespace ГенерацияТВ
             {
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("17.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
-
-
+                
             }
 
             private void gen18()
             {
+                double part1, part2, part3, part4, part5, part6,Mn,ME,Dn,DE,ME0n;
+
+                part1 = r.Next(1, 8);
+                part2 = r.Next(1, 10 - (int)part1);
+                part3 = 10 - part1 - part2;
+                part4 = r.Next(0, (int)part1)/10d;
+                part1 = Math.Abs(part1/10d - part4);
+                part5 = r.Next(0, (int)part2) / 10d;
+                part2 = Math.Abs(part2/10d - part5);
+                part6 = r.Next(0, (int)part3) / 10d;
+                part3 = Math.Abs(part3 / 10d - part6);
                 paragraph = document.InsertParagraph();
                 paragraph.AppendLine("18.  ").Font("Century Schoolbook").FontSize(12).Bold().Alignment = Alignment.left;
+                paragraph.Append(" Дана таблица распределения вероятностей двумерной случай­ной величины  (E,n?)").FontSize(12);
+                Table table = document.AddTable(3, 4);
+                table.Alignment = Alignment.left;
+                table.Rows[0].Cells[0].Paragraphs[0].Append("E,n");
+                table.Rows[0].Cells[1].Paragraphs[0].Append("-1");
+                table.Rows[0].Cells[2].Paragraphs[0].Append("0");
+                table.Rows[0].Cells[3].Paragraphs[0].Append("1");
+                table.Rows[1].Cells[0].Paragraphs[0].Append("0");
+                table.Rows[2].Cells[0].Paragraphs[0].Append("1");
+                table.Rows[1].Cells[1].Paragraphs[0].Append(part1.ToString());
+                table.Rows[1].Cells[2].Paragraphs[0].Append(part2.ToString());
+                table.Rows[1].Cells[3].Paragraphs[0].Append(part3.ToString());
+                table.Rows[2].Cells[1].Paragraphs[0].Append(part4.ToString());
+                table.Rows[2].Cells[2].Paragraphs[0].Append(part5.ToString());
+                table.Rows[2].Cells[3].Paragraphs[0].Append(part6.ToString());
+
+                paragraph.InsertTableAfterSelf(table);
+
+                
 
 
             }
@@ -320,5 +363,6 @@ namespace ГенерацияТВ
             Gen gen = new Gen(countVariants);
 
         }
+        
     }
 }
